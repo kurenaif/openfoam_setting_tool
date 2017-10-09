f23decca57e5c655412d7bc6b77c04433791c367
kurenaif <antyobido@gmail.com>
2017-10-09 23:20:50 +0900

    add directory walk

--- index.js
+++ index.js
@@ -14,3 +14,34 @@ $(function(){
     } });
     $('div.split-pane').splitPane();
 });
+
+var fs = require("fs")
+var path = require("path")
+var dir = '.'; //引数が無いときはカレントディレクトリを対象とする
+
+var walk = function(p, fileCallback, errCallback) {
+
+fs.readdir(p, function(err, files) {
+    if (err) {
+        errCallback(err);
+        return;
+    }
+
+    files.forEach(function(f) {
+        var fp = path.join(p, f); // to full-path
+        if(fs.statSync(fp).isDirectory()) {
+            walk(fp, fileCallback); // ディレクトリなら再帰
+        } else {
+            fileCallback(fp); // ファイルならコールバックで通知
+        }
+    });
+});
+};
+
+
+// 使う方
+walk(dir, function(path) {
+console.log(path); // ファイル１つ受信  
+}, function(err) {
+// alert("Receive err:" + err); // エラー受信
+});
