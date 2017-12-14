4e864633699f82d9a3883e7d89028b7148ff3120
kurenaif <antyobido@gmail.com>
2017-12-14 09:56:18 +0900

    [add] eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true,
-		"node": true
+        "commonjs": true,
+        "es6": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
